3ab766534ee432fd548e592e64f24cd1d1ce70f5
Lavanya Kasturi <lavanyakasturi@gmail.com>
2022-04-07 15:24:45 -0700

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,30 @@
+// Below is a code sample to store person's first name, last name and age using no argument constructor. 
+
+public class Person {
+        String first_name = NULL;
+        String last_name = NULL;
+        Integrer age = -1;
+
+    public Person() {   //Costructor with arguments
+
+    }
+
+    public void setFirstName(String first_name) {
+        this.first_name = first_name;
+    }
+     public void setFirstName(String last_name) {
+        this.last_name = last_name;
+    }
+     public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    public String getLastName() {
+        return last_name;
+    public String age() {
+        return age;
+
+    }
+}
