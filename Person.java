c929898cc2b4a395800cf82f5253604a0eca8482
Lavanya Kasturi <lavanyakasturi@gmail.com>
2022-04-07 23:17:34 -0700

    Fix getter and setter and string types

--- Person.java
+++ Person.java
@@ -3,27 +3,33 @@
 public class Person {
         String first_name = NULL;
         String last_name = NULL;
-        Integrer age = -1;
+        int age = -1;
 
     public Person() {   //Costructor with arguments
-
+            // we use getter setter to set the values in this case
     }
 
     public void setFirstName(String first_name) {
         this.first_name = first_name;
     }
-     public void setFirstName(String last_name) {
-        this.last_name = last_name;
-    }
-     public void setAge(String age) {
-        this.age = age;
-    }
-
+    
     public String getFirstName() {
         return first_name;
+    }
+    
+    public void setFirstName(String last_name) {
+        this.last_name = last_name;
+    }
+     
     public String getLastName() {
         return last_name;
-    public String age() {
+    }   
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+        
+    public int age() {
         return age;
 
     }
